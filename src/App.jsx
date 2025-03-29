e3c906a5e82a2d935b283ee30483bd069e557af2
Dimeyrg <96664094+Dimeyrg@users.noreply.github.com>
2025-03-29 21:13:36 +0300

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TimerApp from './TimerApp';
-
-function App() {
-  return <TimerApp />;
-}
-
-export default App;
--- src/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import timerReducer from './timerSlice';
-
-export const store = configureStore({
-  reducer: {
-    timer: timerReducer,
-  },
-});
